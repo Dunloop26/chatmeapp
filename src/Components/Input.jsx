d214f676160a6b9b01e9b83dc2517ba6c23f2c98
dunas26 <jhonjamesillo@gmail.com>
2022-08-10 23:56:07 -0500

    UPDATE: Name can be changed

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,8 +1,8 @@
-export function Input({label = "", type = "", placeholder = ""}) {
+export function Input({ label = "", type = "", placeholder = "", onValueChange = () => { } }) {
 	return (
 		<div className="input-element">
 			{(label.length > 0 && <label htmlFor="input">{label}</label>)}
-			<input id="input" type={type} placeholder={placeholder} />
+			<input id="input" type={type} placeholder={placeholder} onChange={onValueChange} />
 		</div>
 	)
 }
--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -1,21 +1,38 @@
+import { useState } from 'react'
 import { Button } from '../Components/Button'
 import { Input } from '../Components/Input'
 
 export function Dashboard() {
+	const [name, setName] = useState("")
+	let tempName = "";
+	const assignName = function() {
+		setName(tempName);
+	}
+	const nameInputValueChange = function(e) {
+		const { value } = e.target
+		tempName = value
+	}
+	const welcomeMessage = function() {
+		const solveName = () => {
+			return name.length > 0 ? ` ${name}` : "";
+		}
+		return "Welcome" + solveName() + "!"
+	}
 	return (
 		<div>
 			<header>
 				<h1>Dashboard</h1>
 			</header>
 			<main>
-				<h2>Welcome user</h2>
+				<h2>{welcomeMessage()}</h2>
 				<section>
+					<section className="name-input">
+						<Input label="Nombre" placeholder="Ej.: Fulanito de tal" onValueChange={nameInputValueChange} />
+						<Button text="Asignar" onClick={assignName} />
+					</section>
 					<section className="button-group">
 						<Input label="Código de la reunión" placeholder="Ej.: ABC-123" />
 						<Button text="Unirse" />
-					</section>
-					<section className="name-input">
-						<Input label="Nombre" placeholder="Ingrese un nombre" />
 					</section>
 				</section>
 			</main>
