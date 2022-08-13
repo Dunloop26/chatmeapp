0af2fbdef6258c85687304c410476508405e4c78
dunas26 <jhonjamesillo@gmail.com>
2022-08-12 23:26:31 -0500

    NEW: Current User behaviour

--- src/Layouts/MessageList.jsx
+++ src/Layouts/MessageList.jsx
@@ -1,12 +1,15 @@
+import { useContext } from "react"
 import { Message } from "@Components/Message"
+import GlobalContext from '@Contexts/Global'
 
 export function MessageList({ messages = [] }) {
+	const { currentUser } = useContext(GlobalContext)
 	return (
 		<section>
 			{
 				messages.length > 0
 					? (messages.map(({ author, message }, index) => {
-						return <Message key={index} message={message} author={author} />
+						return <Message key={index} message={message} author={author == currentUser ? "Yo" : author} />
 					}))
 					: <p>No hay mensajes que mostrar</p>
 			}
